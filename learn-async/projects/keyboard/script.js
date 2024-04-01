029d76a08562bad8bd38ad4b1999287109d59ee3
luvsense <yvsh7.btw@gmail.com>
2024-04-02 01:16:55 +0530

    keyboard project

---
+++ learn-async/projects/keyboard/script.js
@@ -0,0 +1,18 @@
+const insert = document.querySelector('#insert');
+window.addEventListener('keydown', function (e) {
+  insert.innerHTML = `
+  <div class = 'color'>
+      <table>
+      <tr>
+      <th>Key</th>
+      <th>Keycode</th>
+      <th>Code</th>
+      </tr>
+      <tr>
+      <th>${e.key === ' ' ? 'space' : e.key}</th>
+      <th>${e.keyCode}</th>
+      <th>${e.code}</th>
+      </tr>
+      </table>
+  </div>`;
+});
